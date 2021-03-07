703eab918914dea0aafa5a0a0198ea7feec3c392
charakuri8 <charakuri8@gmail.com>
2021-03-07 22:35:52 +0900

    20210307 shimamura commit

--- lib/screens/user.dart
+++ lib/screens/user.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hatarakujikan_app/providers/user.dart';
 import 'package:hatarakujikan_app/screens/login.dart';
+import 'package:provider/provider.dart';
 
 class UserScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final userProvider = Provider.of<UserProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         elevation: 0.0,
@@ -20,6 +24,7 @@ class UserScreen extends StatelessWidget {
       body: Center(
         child: TextButton(
           onPressed: () {
+            userProvider.signOut();
             Navigator.pushReplacement(
               context,
               MaterialPageRoute(
