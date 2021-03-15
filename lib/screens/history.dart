975ae7337ba7837656b1ec65b8a60a4911ced47b
agoracreation <agora.creation.com@gmail.com>
2021-03-15 19:00:33 +0900

    20210315 agora commit

--- lib/screens/history.dart
+++ lib/screens/history.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class HistoryScreen extends StatelessWidget {
+class HistoryScreen extends StatefulWidget {
+  @override
+  _HistoryScreenState createState() => _HistoryScreenState();
+}
+
+class _HistoryScreenState extends State<HistoryScreen> {
+  List<DateTime> days = [];
+
+  void _generateDays() {
+    DateTime _start = DateTime.now();
+    DateTime _end = DateTime.now().add(Duration(days: 31));
+    final _daysToGenerate = _end.difference(_start).inDays;
+    days = List.generate(_daysToGenerate,
+        (i) => DateTime(_start.year, _start.month, _start.day + (i)));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _generateDays();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -9,6 +31,28 @@ class HistoryScreen extends StatelessWidget {
         Text('年月選択'),
         SizedBox(height: 8.0),
         Divider(height: 1.0, color: Colors.grey),
+        ListView.builder(
+          shrinkWrap: true,
+          physics: ScrollPhysics(),
+          itemCount: days.length,
+          itemBuilder: (_, index) {
+            return Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(
+                    width: 1.0,
+                    color: Colors.grey.shade300,
+                  ),
+                ),
+              ),
+              child: ListTile(
+                leading:
+                    Text('${DateFormat('dd (E)', 'ja').format(days[index])}'),
+                title: Text('11:00'),
+              ),
+            );
+          },
+        ),
       ],
     );
   }
