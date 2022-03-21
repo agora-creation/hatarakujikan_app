55fad04f769c1c1a6adce4fd86c6e4796fc63d98
charakuri8 <charakuri8@gmail.com>
2022-03-21 19:48:18 +0900

    20220321 commit

--- android/app/src/main/kotlin/com/agoracreation/hatarakujikan_app/Application.kt
+++ android/app/src/main/kotlin/com/agoracreation/hatarakujikan_app/Application.kt
@@ -1,19 +1,6 @@
 package com.agoracreation.hatarakujikan_app
 
 import io.flutter.app.FlutterApplication
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugin.common.PluginRegistry
-import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
-import io.flutter.plugins.GeneratedPluginRegistrant
-import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService
-import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin
 
-class Application : FlutterApplication(), PluginRegistrantCallback {
-    override fun onCreate() {
-        super.onCreate()
-        FlutterFirebaseMessagingService.setPluginRegistrant(this)
-    }
-    override fun registerWith(registry: PluginRegistry?) {
-        FirebaseCloudMessagingPluginRegistrant.registerWith(registry)
-    }
+class Application : FlutterApplication() {
 }
